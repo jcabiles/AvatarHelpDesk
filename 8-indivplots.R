66f906561fafb8654ced277393fe60e7942fead7
John Cabiles <johncabiles@gmail.com>
2019-01-20 22:41:43 -0800

    simplify function that shows plot of specific person's tickets

--- 8-indivplots.R
+++ 8-indivplots.R
@@ -1,25 +1,35 @@
 library(ggplot2)
 library(dplyr)
-
-ia.name <- "Kennedy"
-
-individual.tickets <- tickets %>%
-  filter(FirstSupport %like% ia.name |
-         SecondSupport %like%ia.name |
-           ThirdSupport %like% ia.name |
-           FourthSupport %like% ia.name,
-         
-         # remove duplicates
-         !duplicated(ReferenceNumber))
+library(DescTools)  #contains %like%
 
 
 
-# individual ticket per month
-# bar graph tickets completed
-ggplot(data = subset(individual.tickets,
-                     !is.na(MonthofCloseDate)), aes(x=MonthofCloseDate)) +
-  geom_bar(fill = "blue") + 
-  ylab("Number of Tickets") + 
-  xlab("Month") +
-  ggtitle("Number of Tickets Completed") +
-  theme(plot.title = element_text(hjust=0.5, size=18))
+## define function to look up trainee's tickets
+person.tickets <- function(name){
+  name <- deparse(substitute(name))  # converts name object into a string of the person's name
+  
+  # gather all records containing that person's name
+  individual.tickets <- tickets %>%
+    filter(FirstSupport %like% name |
+             SecondSupport %like% name |
+             ThirdSupport %like% name |
+             FourthSupport %like% name,
+           
+           # remove duplicates
+           !duplicated(ReferenceNumber))
+  
+  
+  # individual ticket per month
+  # bar graph tickets completed
+  ggplot(data = subset(individual.tickets,
+                       !is.na(MonthofCloseDate)), aes(x=MonthofCloseDate)) +
+    geom_bar(fill = "blue") + 
+    ylab("Number of Tickets") + 
+    xlab("Month") +
+    ggtitle("Number of Tickets Completed") +
+    theme(plot.title = element_text(hjust=0.5, size=18))
+}
+
+
+
+
